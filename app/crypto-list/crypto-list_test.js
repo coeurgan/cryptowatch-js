7b535332eed2d4633e53162dc26eaa9d971568fa
Coeurgan <coeurgan@gmail.com>
2018-02-20 15:28:16 +0100

    more renaming

--- app/crypto-list/crypto-list_test.js
+++ app/crypto-list/crypto-list_test.js
@@ -1,15 +1,15 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('myApp.cryptoList module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('myApp.cryptoList'));
 
-  describe('view1 controller', function(){
+  describe('cryptoList controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+      var cryptoListCtrl = $controller(CryptoListCtrl');
+      expect(cryptoListCtrl).toBeDefined();
     }));
 
   });
