4fa6d7d47f8959a2339d2c2ba00e7a770a089d4b
Johann <coeurgan@gmail.com>
2018-02-27 17:38:59 +0100

    tests

--- app/crypto-detail/crypto-detail_test.js
+++ app/crypto-detail/crypto-detail_test.js
@@ -1,83 +1,6 @@
 'use strict';
 
 describe('myApp.cryptoDetail module', function() {
-<<<<<<< HEAD
-	var coinListServiceMock;
-	beforeEach(module('myApp.cryptoDetail'));
-	beforeEach(function() {
-		coinListServiceMock = {
-			getCoins : function() {
-				return [ 			
-					{ code : "XRB", quantity : "10", target : "10000" }, 
-					{ code : "XBY", quantity : "100", target : "5000" },
-					{ code : "ETH", quantity : "1", target : "200000" }
-			]	;
-			},
-		};
-	});
-
-  describe('CryptoDetailCtrl controller', function(){
-<<<<<<< HEAD
-
-	it('should be defined', inject(function($controller) {
-=======
-/*
-    it('should be defined', inject(function($controller) {
-      //spec body
->>>>>>> 644b5c2cd8e394ab495260795b5cb6cd16310f1a
-		var $scope = {};
-		var $rootScope = {};
-		var $routeParams = {};
-		$routeParams.code = 'XRB';
-		var view2Ctrl = $controller('CryptoDetailCtrl', { $scope: $scope, $rootScope:$rootScope, $routeParams:$routeParams,coinListService:coinListServiceMock });
-		expect(view2Ctrl).toBeDefined();
-		expect($scope.isEdit).toBeDefined();
-		expect($scope.isEdit).toBe(false);
-	}));
-
-	it('should provide the selected coin', inject(function($controller) {
-		var $scope = {};
-		var $rootScope = {};
-		var $routeParams = {};
-		$routeParams.code = 'XBY';
-<<<<<<< HEAD
-
-		var view2Ctrl = $controller('CryptoDetailCtrl', { $scope: $scope, $rootScope:$rootScope, $routeParams:$routeParams ,coinListService:coinListServiceMock});
-		expect(view2Ctrl).toBeDefined();
-		expect($scope.coin.code).toBe("XBY");
-		expect($scope.coin.quantity).toBe("100");
-		expect($scope.coin.target).toBe("5000");
-	}));
-	
-	it('should make the editable fields visible', inject(function($controller) {
-		var $scope = {};
-		var $rootScope = {};
-		var $routeParams = {};
-		$routeParams.code = 'XBY';
-
-		var view2Ctrl = $controller('CryptoDetailCtrl', { $scope: $scope, $rootScope:$rootScope, $routeParams:$routeParams ,coinListService:coinListServiceMock});
-		expect(view2Ctrl).toBeDefined();
-
-		$scope .edit();
-		expect($scope.isEdit).toBe(true);
-	}));
-
-
-=======
-		$rootScope.coins = [		
-			{ code : "XRB", quantity : "10", target : "10000" }, 
-			{ code : "XBY", quantity : "100", target : "5000" },
-			{ code : "ETH", quantity : "1", target : "200000" }
-		];
-      var view2Ctrl = $controller('CryptoDetailCtrl', { $scope: $scope, $rootScope:$rootScope, $routeParams:$routeParams });
-      expect(view2Ctrl).toBeDefined();
-	  expect($scope.coin.code).toBe("XBY");
-	  expect($scope.coin.quantity).toBe("100");
-	  expect($scope.coin.target).toBe("5000");
-    }));
-*/
->>>>>>> 644b5c2cd8e394ab495260795b5cb6cd16310f1a
-=======
 
     var service, mockHttp;
     beforeEach(module('myApp.cryptoList'));
@@ -119,8 +42,18 @@ describe('myApp.cryptoDetail module', function() {
         expect($scope.coin.quantity).toBe("100");
         expect($scope.coin.target).toBe("5000");
     }));
+    
+    	it('should make the editable fields visible', inject(function($controller) {
+		var $scope = {};
+		var $rootScope = {};
+		var $routeParams = {};
+		$routeParams.code = 'XBY';
 
+		var view2Ctrl = $controller('CryptoDetailCtrl', { $scope: $scope, $rootScope:$rootScope, $routeParams:$routeParams ,coinListService:service});
+		expect(view2Ctrl).toBeDefined();
 
->>>>>>> 6da352ac2e9d9bfd3c2a9a1a4201953157ef5b30
+		$scope .edit();
+		expect($scope.isEdit).toBe(true);
+	}));
   });
 });
